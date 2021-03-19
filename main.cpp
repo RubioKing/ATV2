2e21827138341725f3979e03e065f53a7ff975a3
Matheus Rubio <matheus.rubio@ice.ufjf.br>
2021-03-19 18:23:48 -0300

    Init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    vector<string> dicionario;
+    string entrada = "bananabanabofana";
+
+    for (int i = 0; i < entrada.length(); i++)
+    {
+        char c = entrada[i+1];
+        dicionario.
+        if(entrada + c dicionario.)
+    }
+
+    return 0;
+}
